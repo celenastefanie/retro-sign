748129dffa6ac4067df3c209d9b789c12379192e
celenastefanie <celenastefanie@gmail.com>
2016-04-02 20:52:03 -0400

    i think things are making sense?

--- js/main.js
+++ js/main.js
@@ -2,6 +2,12 @@ var $btn = $('.btn');
 var $lines = $('.lines');
 var $engage = $('.engage');
 
-$btn.click('click', function (e) {
+$btn.on('click', function (e) {
   $btn.toggleClass('btn-click');
+  $lines.toggleClass('add-lines');
+  $btn.toggleClass('word-change');
+});
+
+$btn.on('mouseover', function (e) {
+  $btn.toggleClass.children('path:last-child').css('fill', 'orange');
 });
